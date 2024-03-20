5a1eaa29a95bccf12d4cc96a4c0a130fa1acd80c
Snekhal Prasad <112121062@nitt.edu>
2024-03-21 02:21:10 +0530

    Revert

--- server/index.js
+++ server/index.js
@@ -15,25 +15,16 @@ import {kpis, products, transactions} from "./data/data.js";
 
 
 // CONFIGURATIONS
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const cors = require("cors");
 dotenv.config();
+const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(cors());
+app.use(cors());
 
-
-//middleware
-const corsOptions = {
-    origin: "https://growdha-website.onrender.com/",
-}
-app.use(cors(corsOptions));
 
 //ROUTES
 app.use("/kpi", kpiRoutes);
@@ -56,9 +47,4 @@ mongoose
 
   })
   .catch((error) => console.log(`${error} did not connect`))
-
-  //Backend route
-app.get("/", (req, res) => {
-    res.status(201).json({message: "Connected to Backend!"});
-});
 
