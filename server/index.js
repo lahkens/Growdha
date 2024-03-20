c0e8f2ce194eb34a2953d38af2c3dbebb9e24c5d
Snekhal Prasad <112121062@nitt.edu>
2024-03-21 02:52:30 +0530

    Error Handling

--- server/index.js
+++ server/index.js
@@ -5,55 +5,48 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import kpiRoutes from "./routes/kpi.js"
+import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
-import transactionRoutes from "./routes/transaction.js"
+import transactionRoutes from "./routes/transaction.js";
 import Product from "./models/Product.js";
 import KPI from "./models/KPI.js";
-import Transaction from "./models/Transaction.js"
-import {kpis, products, transactions} from "./data/data.js";
-
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 // CONFIGURATIONS
 dotenv.config();
 const app = express();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
 // middleware
 const corsOptions = {
     origin: "https://growdha-website.onrender.com/",
-}
+};
 
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors(corsOptions));
 
-//ROUTES
+// ROUTES
 app.use("/kpi", kpiRoutes);
-app.use("/product", productRoutes)
-app.use("/transaction", transactionRoutes)
-
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 // MONGOOSE SETUP
-const PORT = process.env.PORT || 3000;
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(async () => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-      
-    //Add Only Once
-    // await mongoose.connection.db.dropDatabase();
-    // KPI.insertMany(kpis);
-    // Product.insertMany(products);
-    // Transaction.insertMany(transactions);
+    .connect(process.env.MONGO_URL)
+    .then(async () => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-  })
-  .catch((error) => console.log(`${error} did not connect`))
+        // Add Only Once
+        // await mongoose.connection.db.dropDatabase();
+        // KPI.insertMany(kpis);
+        // Product.insertMany(products);
+        // Transaction.insertMany(transactions);
 
+    })
+    .catch((error) => console.log(`${error} did not connect`));
